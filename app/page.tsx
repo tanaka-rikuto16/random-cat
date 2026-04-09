265eef941e8b932aeaa8563426e594fc67cdb6bb
Tanaka Rikuto <r-tanaka@HP400G9-ISI.rnc-com.co.jp>
2026-04-09 16:18:19 +0900

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { connection } from "next/server";
+import { CatImage } from "./cat-image";
+import { fetchImage } from "./fetch-image";
+
+export default async function Home(){
+  await connection();
+  const image = await fetchImage();
+  return <CatImage url={image.url} />;
+}
